5437053d6f6d02db787ea0eef320ef7676a750b5
fanyk <fanyk@ZBOOK-FANNY>
2020-09-27 16:48:34 +0200

    InputCheckList début

---
+++ src/features/ui/InputCheckList.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { InputCheckbox } from 'freeassofront';
+import { AddOne as AddOneIcon } from '../icons';
+
+export default class InputCheckList extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    items: PropTypes.array,
+  };
+
+  static defaultProps = {
+    title: "Titre",
+    items: [],
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+    this.addOne = this.addOne.bind(this);
+    this.delOne = this.delOne.bind(this);
+  }
+
+  addOne() {
+    let { items } = this.props;
+    if (!Array.isArray(items)) {
+      items = [];
+    }
+    let index = this.state.index;
+    index = index - 1;
+  }
+
+  delOne(i) {
+    let { items } = this.props;
+    items.splice(i, 1);
+    this.props.onChange({
+      target: {
+        name: this.props.name,
+        value: items,
+      },
+    });
+  }
+
+  render() {
+    
+    if (Array.isArray(this.props.items)) {
+      this.props.items.map((elem, i) => {
+        console.log("FK items", elem.text);
+      })
+    }
+    return (
+      <div className="input-check-list">
+        {this.props.items.map((elem, i) => {
+          <div className='row'>
+            <InputCheckbox
+              label={elem.text}
+              name={i}
+              labelTop={false}
+              checked={elem.done === true}
+            />
+          </div>
+        })}
+        <div className="col-9">
+          <button className="btn btn-primary" onClick={this.addOne}>
+            <AddOneIcon />
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
