8a52d44d3f990f7cdc857ca60847144a885b8d93
jeromeklam <jeromeklam@free.fr>
2020-10-11 14:17:31 +0200

    Cordova

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import Root from './Root';
 import * as serviceWorker from './serviceWorker';
 import './styles/index.scss';
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const renderReactDom = () => {
+  ReactDOM.render(<Root />, document.getElementById('root'));
+};
+
+if (window.cordova) {
+  document.addEventListener('deviceready', () => {
+    renderReactDom();
+  }, false);
+} else {
+  renderReactDom();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
